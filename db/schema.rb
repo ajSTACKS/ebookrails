ab91d847c4d99899d077d6736483b6c8a6be5445
a j <>
2018-10-17 03:28:36 -0700

    generate books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_011221) do
+ActiveRecord::Schema.define(version: 2018_10_17_094538) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "description"
+    t.integer "price"
+    t.boolean "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
