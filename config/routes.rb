caa316cd5e5e46fd0c8dcc35df6909eb24c4f254
a j <>
2018-10-18 21:38:53 -0700

    add dashboard to current user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
+
+  get 'dashboard' => 'pages#dashboard'
 end
   root to: 'pages#home'
   
